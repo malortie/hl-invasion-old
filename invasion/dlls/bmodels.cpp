8c9580fab61a20248e7d04bd442f3df4d84840b9
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-01-02 12:36:47 -0500

    Added SF_CONVEYOR_ONOFF definition.
    Appears to be unused. Added here to match original source code.

--- invasion/dlls/bmodels.cpp
+++ invasion/dlls/bmodels.cpp
@@ -143,6 +143,7 @@ void CFuncWallToggle :: Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_
 
 #define SF_CONVEYOR_VISUAL		0x0001
 #define SF_CONVEYOR_NOTSOLID	0x0002
+#define SF_CONVEYOR_ONOFF		8
 
 class CFuncConveyor : public CFuncWall
 {
